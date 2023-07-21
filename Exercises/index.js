61722fdcfd9978b174dd9f792b7c251d61a59271
modar_algayal <mudar.algayal@helsinki.fi>
2023-07-21 12:35:17 +0300

    update

--- Exercises/index.js
+++ Exercises/index.js
@@ -20,6 +20,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') 
+    {return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -101,7 +103,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -136,16 +138,15 @@ app.post('/api/persons', (request, response) => {
 
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
+
+  Person.findByIdAndUpdate(request.params.id,
+    { name }, 
+    { new: true , runValidators: true, context: 'query'})
+    .then(updatedName => {
+      response.json(updatedName)
     })
     .catch(error => next(error))
 })
