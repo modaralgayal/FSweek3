34fd5f6e073b667c738d4044c83ed4f52a709c8f
modar_algayal <mudar.algayal@helsinki.fi>
2023-07-22 21:53:57 +0300

    update

--- Exercises/models/person.js
+++ Exercises/models/person.js
@@ -22,8 +22,18 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 3,
   },
-  number: Number,
-})
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{3}-\d{5,}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
+  })
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
